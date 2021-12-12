3356d2511d3b8ef9c629c11877a60e238bc744ea
LI_XE <hyangeun312@gmail.com>
2021-12-12 02:04:37 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './index.css';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All right is reserved.
+      </footer>
+    </div>
+  );
+}
+
+export default App;
